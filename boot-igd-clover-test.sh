3a615cb0d80dd9b13325495d8550110de33ffdbf
Max Lansing <lansing@gmail.com>
2019-03-22 13:15:55 -0700

    last working clvoer test without igd

--- boot-igd-clover-test.sh
+++ boot-igd-clover-test.sh
@@ -14,10 +14,12 @@ LD_LIBRARY_PATH=/usr/local/lib $QEMU_SYSTEM \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
 	  -smbios type=2 \
 	  -device ahci,id=ahci \
-    -drive id=disk0,file=/home/max/Code/OSX-IGD/clover-empty.qcow2,if=none,format=qcow2 \
+    -drive id=disk0,file=/home/max/Code/OSX-IGD/clover-setup.qcow2,if=none,format=qcow2 \
     -device ide-drive,drive=disk0,bus=ahci.0 \
+    -drive id=disk1,file=/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77826366B7,if=none,format=raw \
+    -device ide-drive,drive=disk1,bus=ahci.1 \
     -chardev file,id=seabios,path=/tmp/bios.log \
-    -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,addr=0x05,mac=52:54:00:c9:18:27 \
+    -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,addr=0x07,mac=52:54:00:c9:18:27 \
     -device isa-debugcon,iobase=0x402,chardev=seabios \
     -usb \
     -device usb-host,vendorid=0x1ea7,productid=0x0066 \
