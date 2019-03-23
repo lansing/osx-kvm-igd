052cccfa26b4d21a3d4ae9b84a107d6c177ef653
Max Lansing <lansing@gmail.com>
2019-03-23 09:26:57 -0700

    remove qcow to commit
    Former-commit-id: 2c1e1aa55e4945800f0381a13863997d2d28705c
    Former-commit-id: 62a4c59b12cc4dbf081b7a8fe252e23e0bfc5f00

--- boot-igd.sh
+++ boot-igd.sh
@@ -1,22 +1,26 @@
 MY_OPTIONS="+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 
 QEMU_SYSTEM=qemu-system-x86_64
+MY_OPTIONS="+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
+
+QEMU_SYSTEM=qemu-system-x86_64
 
 # using qemu v4.0.0-rc0 right now, built with libusb v1.0.19-442-g2a7372d
 QEMU_LATEST=/home/max/Code/qemu/x86_64-softmmu/qemu-system-x86_64
 
-
-LD_LIBRARY_PATH=/usr/local/lib $QEMU_LATEST \
-    -enable-kvm -m 10000 \
-    -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on \
+LD_LIBRARY_PATH=/usr/local/lib $QEMU_SYSTEM \
+    -enable-kvm -m 16384 \
+    -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS \
 	  -machine pc  \
-	  -smp 4,cores=2 \
+	  -smp 12,cores=2 \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
 	  -smbios type=2 \
-    -device vfio-pci,host=00:02.0,bus=pci.0,addr=0x2,x-igd-opregion=on \
-	  -device ahci,id=ahci \
-    -drive id=disk0,file=/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77826366B7,if=none,format=raw \
+    -device vfio-pci,host=00:02.0,bus=pci.0,addr=0x2,x-igd-opregion=on,romfile=/home/max/Code/OSX-IGD/vbios-fixed.dump \
+	  -device ahci,id=ahci,addr=0x07 \
+    -drive id=disk0,file=/home/max/Code/OSX-IGD/clover-setup.qcow2,if=none,format=qcow2 \
     -device ide-drive,drive=disk0,bus=ahci.0 \
+    -drive id=disk1,file=/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77826366B7,if=none,format=raw \
+    -device ide-drive,drive=disk1,bus=ahci.1 \
     -chardev file,id=seabios,path=/tmp/bios.log \
     -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,addr=0x05,mac=52:54:00:c9:18:27 \
     -device isa-debugcon,iobase=0x402,chardev=seabios \
@@ -26,3 +30,4 @@ LD_LIBRARY_PATH=/usr/local/lib $QEMU_LATEST \
     -vga none \
     -serial none \
     -nographic
+
