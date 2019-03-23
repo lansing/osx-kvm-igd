1c507fa30795aaf9bb84aabd7d6945fc3f3e1a8d
Max Lansing <lansing@gmail.com>
2019-03-22 19:02:27 -0700

    back on system qemu
    Former-commit-id: 5449f66821d47da36331ea30eca3dedded98406e
    Former-commit-id: ad2bde4b089afefe479a33183305b137383555ab

--- boot-igd-clover-test.sh
+++ boot-igd-clover-test.sh
@@ -8,15 +8,15 @@ QEMU_SYSTEM=qemu-system-x86_64
 # using qemu v4.0.0-rc0 right now, built with libusb v1.0.19-442-g2a7372d
 QEMU_LATEST=/home/max/Code/qemu/x86_64-softmmu/qemu-system-x86_64
 
-LD_LIBRARY_PATH=/usr/local/lib $QEMU_LATEST \
-    -enable-kvm -m 10000 \
-    -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on \
+LD_LIBRARY_PATH=/usr/local/lib $QEMU_SYSTEM \
+    -enable-kvm -m 16384 \
+    -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS \
 	  -machine pc  \
-	  -smp 4,cores=2 \
+	  -smp 8,cores=2 \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
 	  -smbios type=2 \
-    -device vfio-pci,host=00:02.0,bus=pci.0,addr=0x2,x-igd-opregion=on \
-	  -device ahci,id=ahci \
+    -device vfio-pci,host=00:02.0,bus=pci.0,addr=0x2,x-igd-opregion=on,romfile=/home/max/Code/OSX-IGD/vbios-fixed.dump \
+	  -device ahci,id=ahci,addr=0x07 \
     -drive id=disk0,file=/home/max/Code/OSX-IGD/clover-setup.qcow2,if=none,format=qcow2 \
     -device ide-drive,drive=disk0,bus=ahci.0 \
     -drive id=disk1,file=/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77826366B7,if=none,format=raw \
