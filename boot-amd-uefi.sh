1eae88a602fe60709b18778b61929af82f28cc7d
Max Lansing <lansing@gmail.com>
2019-05-17 19:23:09 -0700

    amd tap network

--- boot-amd-uefi.sh
+++ boot-amd-uefi.sh
@@ -18,11 +18,13 @@ qemu-system-x86_64 -enable-kvm -m 16384 -cpu Penryn,kvm=on,vendor=GenuineIntel,+
 	  -smp 12,cores=2 \
 	  -drive if=pflash,format=raw,readonly,file=/home/max/Code/osx-kvm-igd/OVMF/OVMF_CODE.fd \
 	  -drive if=pflash,format=raw,file=/home/max/Code/osx-kvm-igd/OVMF/OVMF_VARS.fd \
-    -device vfio-pci,host=01:00.0,addr=0x16,romfile=/home/max/Code/osx-kvm-igd/wx-4100.rom \
+    -device vfio-pci,host=01:00.0,addr=0x16,multifunction=on,x-vga=on,romfile=/home/max/Code/osx-kvm-igd/wx-4100.rom \
+    -device vfio-pci,host=01:00.1,addr=0x17 \
     -device vfio-pci,host=02:00.0,x-msix-relocation=bar2 \
     -drive id=disk1,file=/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77826366B7,if=none,format=raw \
     -device ide-drive,drive=disk1,bus=ide.0 \
-	  -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,addr=0x05,mac=52:54:00:c9:18:27 \
+    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+    -device vmxnet3,netdev=net0,id=net0,addr=0x05,mac=52:54:00:c9:18:27 \
     -object input-linux,id=mouse1,evdev=/dev/input/by-id/usb-Logitech_USB-PS_2_Trackball-event-mouse \
     -object input-linux,id=kbd1,evdev=/dev/input/by-id/usb-04d9_USB-HID_Keyboard-event-kbd,grab_all=on,repeat=on \
 	  -monitor stdio \
